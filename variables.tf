39110a8c3742a8e88fa1b8687864ad43125cf3f1
cneralich <corrigan@hashicorp.com>
2019-07-10 12:44:33 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,18 @@
-variable "token" {}
+# THESE VALUES CAN BE MANAGED WITHIN THE WORKSPACE ITSELF
+# The Values are unchanging in the 'Master' Workspace
 
-variable "workspace_name" {}
+variable "token" {}
 
 variable "organization" {}
 
 variable "vcs_repo" {}
 
 variable  "oauth_token_id" {}
+
+# The Values can be changed for use in the newly-created Workspace
+# Additional values could be added here (ex. branch, etc.)
+
+variable "workspace_name" {}
 
 variable "tfe_vars_sensitive" {
   type        = "map"
